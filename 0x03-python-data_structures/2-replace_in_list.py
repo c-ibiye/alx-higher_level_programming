cfdc5005f8295791e9f68992f0e7b2cefbb01260
Charles Ibiye <c.ibiye@gmail.com>
2022-09-26 10:25:38 -0700

    2-replace_in_list.py fix

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    #Replaces an element in a list at given index
+    # Replaces an element in a list at given index
     if len(my_list) <= idx or idx < 0:
         return (my_list)
     my_list[idx] = element
