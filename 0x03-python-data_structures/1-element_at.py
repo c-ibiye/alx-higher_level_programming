fe77f995f68c492efc1b07450573e483e46cc3f4
Charles Ibiye <c.ibiye@gmail.com>
2022-09-26 10:09:51 -0700

    1-element_at.py fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    #Gets an element in a list at given index And returns it
+    # Gets an element in a list at given index And returns it
     if idx >= len(my_list) or idx < 0:
         return
 
