d8f4c12668d9454c7451207384f8bf5705c73d7c
Charles Ibiye <c.ibiye@gmail.com>
2022-09-28 13:53:45 -0700

    0-square_matrix_simple.py added

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    # Compute the square value of all integers of a matrix
+    new_matrix = list(map(lambda x: (x**2), matrix))
+    return new_matrix
