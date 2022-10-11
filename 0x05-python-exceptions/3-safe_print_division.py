9ceb5e9b50256824b2f8415a35c5b16cf318bbe5
Charles Ibiye <c.ibiye@gmail.com>
2022-10-11 09:38:28 -0700

    3-safe_print_division.py added

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    # divides two integers and prints the result
+    # catches divide by zero exception
+    try:
+        result = a / b
+    except (TypeError, ZeroDivisionError):
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
