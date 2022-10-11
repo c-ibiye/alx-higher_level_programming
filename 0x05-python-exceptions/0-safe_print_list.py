77b46cf62cdd68d6d148ebf09a8ab294a80882a2
Charles Ibiye <c.ibiye@gmail.com>
2022-10-11 09:01:36 -0700

    0-safe_print_list.py fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             printed += 1
-        except (ValueError, TypeError):
-            continue
+        except IndexError:
+            break
     print()
     return printed
